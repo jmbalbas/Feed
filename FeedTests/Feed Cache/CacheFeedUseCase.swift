433b8d03f3009cb4db64ecf98a51509b7bafebd0
Juan Santiago Martín Balbás <jmbalbas@gmail.com>
2023-01-10 20:06:55 +0100

    Save command requests cache deletion

--- FeedTests/Feed Cache/CacheFeedUseCase.swift
+++ FeedTests/Feed Cache/CacheFeedUseCase.swift
@@ -7,14 +7,25 @@
 
 import Foundation
 import XCTest
+import Feed
 
 class FeedStore {
     private(set) var deleteCachedFeedCallCount: Int = 0
+
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class LocalFeedStore {
-    init(store: FeedStore) {
+    private let store: FeedStore
 
+    init(store: FeedStore) {
+        self.store = store
+    }
+
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
@@ -23,8 +34,28 @@ class CacheFeedUseCase: XCTestCase {
     func test_init_doesNotDeleteCacheUponCreation() {
         let store = FeedStore()
         _ = LocalFeedStore(store: store)
-        
+
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
 
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedStore(store: store)
+        let items: [FeedItem] = [uniqueItem, uniqueItem]
+
+        sut.save(items)
+
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+}
+
+private extension CacheFeedUseCase {
+
+    var uniqueItem: FeedItem {
+        FeedItem(id: UUID(), description: nil, location: nil, imageURL: anyURL)
+    }
+
+    var anyURL: URL {
+        URL(string: "http://any-url.com")!
+    }
 }
