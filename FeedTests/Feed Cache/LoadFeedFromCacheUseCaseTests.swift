31b60d9b779f62b6de0c4168e31b476fb8365dc9
Juan Santiago Martín Balbás <jmbalbas@gmail.com>
2023-04-07 13:52:42 +0200

    Load command delivers cached images on less than seven days old cache

--- FeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ FeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -40,12 +40,37 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
             store.completeRetrievalWithEmptyCache()
         })
     }
+
+    func test_load_deliversCachedImagesOnLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = givenSUT(currentDate: { fixedCurrentDate })
+
+        expect(sut, toCompleteWith: .success(feed.models), when: {
+            store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+        })
+    }
 }
 
 private extension LoadFeedFromCacheUseCaseTests {
 
     var anyNSError: NSError {
         NSError(domain: "Any error", code: 1)
+    }
+
+    var uniqueImage: FeedImage {
+        FeedImage(id: UUID(), description: nil, location: nil, url: anyURL)
+    }
+
+    var uniqueImageFeed: (models: [FeedImage], local: [LocalFeedImage]) {
+        let models: [FeedImage] = [uniqueImage, uniqueImage]
+        let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+        return (models, local)
+    }
+
+    var anyURL: URL {
+        URL(string: "http://any-url.com")!
     }
 
     func givenSUT(
@@ -82,5 +107,15 @@ private extension LoadFeedFromCacheUseCaseTests {
 
         action()
         waitForExpectations(timeout: 0.3)
+    }
+}
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+
+    func adding(seconds: TimeInterval) -> Date {
+        self + seconds
     }
 }
