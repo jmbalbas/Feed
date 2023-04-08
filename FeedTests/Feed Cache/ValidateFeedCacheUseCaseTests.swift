9b835406bff10bfdae5818633b9eb8f419cdba88
Juan Santiago Martín Balbás <jmbalbas@gmail.com>
2023-04-08 19:32:27 +0200

    Validate cache command does not delete less than seven days old cache

--- FeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ FeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -34,12 +34,38 @@ final class ValidateFeedCacheUseCaseTests: XCTestCase {
 
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
+
+    func test_validateCache_doesNotDeleteLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = givenSUT(currentDate: { fixedCurrentDate })
+
+        sut.validateCache()
+        store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
 }
 
 private extension ValidateFeedCacheUseCaseTests {
 
     var anyNSError: NSError {
         NSError(domain: "Any error", code: 1)
+    }
+
+    var uniqueImage: FeedImage {
+        FeedImage(id: UUID(), description: nil, location: nil, url: anyURL)
+    }
+
+    var uniqueImageFeed: (models: [FeedImage], local: [LocalFeedImage]) {
+        let models: [FeedImage] = [uniqueImage, uniqueImage]
+        let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+        return (models, local)
+    }
+
+    var anyURL: URL {
+        URL(string: "http://any-url.com")!
     }
 
     func givenSUT(
@@ -55,3 +81,14 @@ private extension ValidateFeedCacheUseCaseTests {
     }
 
 }
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+
+    func adding(seconds: TimeInterval) -> Date {
+        self + seconds
+    }
+}
+
