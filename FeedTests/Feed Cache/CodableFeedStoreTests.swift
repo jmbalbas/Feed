64c0896f16c0d29a25c7005093fb1b615c07d189
Juan Santiago Martín Balbás <jmbalbas@gmail.com>
2023-08-12 19:00:45 +0200

    Improve test name to follow convention

--- FeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ FeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -84,7 +84,7 @@ final class CodableFeedStoreTests: XCTestCase {
         await expect(sut, toRetrieveTwice: .empty)
     }
     
-    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() async {
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() async {
         let sut = makeSUT()
         let feed = uniqueImageFeed.local
         let timestamp = Date()
