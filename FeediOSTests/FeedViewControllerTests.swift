ce3fb6bf15ad922ac8e1f3ef8bc27ca390e4c67a
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-10-07 13:58:17 +0200

    Show loading indicator on pull to refresh

--- FeediOSTests/FeedViewControllerTests.swift
+++ FeediOSTests/FeedViewControllerTests.swift
@@ -24,12 +24,13 @@ class FeedViewController: UITableViewController {
 
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-        refreshControl?.beginRefreshing()
 
         load()
     }
 
     @objc private func load() {
+        refreshControl?.beginRefreshing()
+
         loader?.load { [weak self] _ in
             self?.refreshControl?.endRefreshing()
         }
@@ -78,6 +79,14 @@ final class FeedViewControllerTests: XCTestCase {
         loader.completeFeedLoading()
 
         XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
+    }
+
+    func test_pullToRefresh_showsLoadingIndicator() {
+        let (sut, _) = makeSUT()
+
+        sut.refreshControl?.simulatePullToRefresh()
+
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
     }
 }
 
