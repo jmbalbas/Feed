e8946a61787fd7b957d33b608a44af052e148011
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-10-14 11:58:59 +0200

    Extract `FeedImageCell` test helpers to a separate file

--- FeediOSTests/Controllers/FeedViewControllerTests.swift
+++ FeediOSTests/Controllers/FeedViewControllerTests.swift
@@ -373,33 +373,3 @@ class LoaderSpy: FeedLoader, FeedImageDataLoader {
         imageRequests[index].completion(.failure(error))
     }
 }
-
-private extension FeedImageCell {
-    var isShowingLocation: Bool {
-        !locationContainer.isHidden
-    }
-
-    var locationText: String? {
-        locationLabel.text
-    }
-
-    var descriptionText: String? {
-        descriptionLabel.text
-    }
-
-    var isShowingImageLoadingIncator: Bool {
-        feedImageContainer.isShimmering
-    }
-
-    var renderedImage: Data? {
-        feedImageView.image?.pngData()
-    }
-
-    var isShowingRetryAction: Bool {
-        !feedImageRetryButton.isHidden
-    }
-
-    func simulateRetryAction() {
-        feedImageRetryButton.simulateTap()
-    }
-}
