b6b5fdd80858436f455deb5ca87c8470b7c44e9f
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-10-17 18:44:00 +0200

    Move ViewModels to separate files

---
+++ FeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  FeediOS
+//
+//  Created by Juan Santiago Martín Balbás on 17/10/23.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ FeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  FeediOS
+//
+//  Created by Juan Santiago Martín Balbás on 17/10/23.
+//
+
+import Feed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
