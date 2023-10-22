7efff73f6a5e34963ccee88556db56586c037022
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-10-22 12:39:21 +0200

    Move `FeedLoaderPresentationAdapter` to separate file

---
+++ FeediOS/Feed UI/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,31 @@
+//
+//  FeedLoaderPresentationAdapter.swift
+//  FeediOS
+//
+//  Created by Juan Santiago Martín Balbás on 22/10/23.
+//
+
+import Feed
+import Foundation
+
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+    private let feedLoader: FeedLoader
+    var presenter: FeedPresenter?
+
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+
+    func didRequestFeedRefresh() {
+        presenter?.didStartLoadingFeed()
+
+        feedLoader.load { [weak presenter] result in
+            switch result {
+            case let .success(feed):
+                presenter?.didFinishLoadingFeed(with: feed)
+            case let .failure(error):
+                presenter?.didFinishLoadingFeed(with: error)
+            }
+        }
+    }
+}
--- FeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ FeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -63,25 +63,3 @@ private final class FeedViewAdapter: FeedView {
         }
     }
 }
-
-private final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
-    private let feedLoader: FeedLoader
-    var presenter: FeedPresenter?
-
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-
-    func didRequestFeedRefresh() {
-        presenter?.didStartLoadingFeed()
-
-        feedLoader.load { [weak presenter] result in
-            switch result {
-            case let .success(feed):
-                presenter?.didFinishLoadingFeed(with: feed)
-            case let .failure(error):
-                presenter?.didFinishLoadingFeed(with: error)
-            }
-        }
-    }
-}
