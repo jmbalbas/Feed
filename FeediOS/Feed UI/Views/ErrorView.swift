d401ef7c5858b722f26ca371235148d226d6c97f
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-10-27 18:34:12 +0200

    Move `ErrorView` to a separate file

---
+++ FeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  FeediOS
+//
+//  Created by Juan Santiago Martín Balbás on 27/10/23.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
