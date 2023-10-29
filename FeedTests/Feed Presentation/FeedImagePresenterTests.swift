6a76b15ce4e38ffa317ae94abc82388dd54b2015
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-10-29 18:36:29 +0100

    Inline `Data` creation

--- FeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ FeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -94,9 +94,8 @@ final class FeedImagePresenterTests: XCTestCase {
     func test_didFinishLoadingImageData_displaysRetryOnFailedImageTransformation() throws {
         let (sut, view) = makeSUT(imageTransformer: fail)
         let image = uniqueImage
-        let data = Data()
 
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with: Data(), for: image)
 
         let messages = view.messages
         XCTAssertEqual(messages.count, 1)
@@ -111,11 +110,10 @@ final class FeedImagePresenterTests: XCTestCase {
 
     func test_didFinishLoadingImageData_displaysImageOnSuccessfulTransformation() throws {
         let image = uniqueImage
-        let data = Data()
         let transformedData = AnyImage()
         let (sut, view) = makeSUT(imageTransformer: { _ in transformedData })
 
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with: Data(), for: image)
 
         let messages = view.messages
         XCTAssertEqual(messages.count, 1)
