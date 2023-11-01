3012440d03dce7629ac4fca2ca276c999ad4ff00
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-11-01 19:18:53 +0100

    `RemoteFeedImageDataLoader` requests data from URL on `loadImageDataFromURL`

--- FeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ FeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -9,19 +9,32 @@ import Feed
 import XCTest
 
 class RemoteFeedImageDataLoader {
-    init(client: Any) {
+    private let client: HTTPClient
 
+    init(client: HTTPClient) {
+        self.client = client
+    }
+
+    func loadImageData(from url: URL, completion: @escaping (Any) -> Void) {
+        client.get(from: url) { _ in }
     }
 }
 
 class RemoteFeedImageDataLoaderTests: XCTestCase {
-
     func test_init_doesNotPerformAnyURLRequest() {
         let (_, client) = makeSUT()
 
         XCTAssert(client.requestedURLs.isEmpty)
     }
 
+    func test_loadImageDataFromURL_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.loadImageData(from: url) { _ in }
+
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
 }
 
 private extension RemoteFeedImageDataLoaderTests {
@@ -38,6 +51,10 @@ private extension RemoteFeedImageDataLoaderTests {
     }
 }
 
-private class HTTPClientSpy {
+private class HTTPClientSpy: HTTPClient {
     var requestedURLs = [URL]()
+
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+        requestedURLs.append(url)
+    }
 }
