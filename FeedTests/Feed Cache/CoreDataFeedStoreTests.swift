0397d1419423dc13b1b538ed3e827f201026a902
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-11-02 13:17:12 +0100

    `CoreDataFeedStore.retrieve()` delivers empty on empty cache

--- FeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ FeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -5,12 +5,14 @@
 //  Created by Juan Santiago Martín Balbás on 2/11/23.
 //
 
+import Feed
 import XCTest
 
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
-
     func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
 
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
 
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
@@ -57,4 +59,12 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
 
     }
 
+}
+
+private extension CoreDataFeedStoreTests {
+    func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+        let sut = CoreDataFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
 }
