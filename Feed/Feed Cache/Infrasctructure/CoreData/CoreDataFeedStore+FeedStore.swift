0ec6eece8759ec65a7b5d596a00aad39ff036e90
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-11-02 15:47:02 +0100

    Move `CoreDataFeedStore` implementation of the `FeedStore` protocol to separate file

---
+++ Feed/Feed Cache/Infrasctructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -0,0 +1,50 @@
+//
+//  CoreDataFeedStore+FeedStore.swift
+//  Feed
+//
+//  Created by Juan Santiago Martín Balbás on 2/11/23.
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedStore {
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        perform { context in
+            do {
+                if let cache = try ManagedCache.find(in: context) {
+                    completion(.success(CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp)))
+                } else {
+                    completion(.success(.none))
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        perform { context in
+            do {
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
+
+                try context.save()
+                completion(.success(()))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        perform { context in
+            do {
+                try ManagedCache.find(in: context).map(context.delete).map(context.save)
+                completion(.success(()))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+}
