a94c0b665658047dc395ad82d94090220d09903d
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-11-04 16:00:10 +0100

    `FeedLoaderWithFallbackComposite.load` delivers fallback feed on primary loader failure

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackComposite.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackComposite.swift
@@ -19,7 +19,14 @@ final class FeedLoaderWithFallbackComposite: FeedLoader {
     }
 
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        primary.load(completion: completion)
+        primary.load { [weak self] result in
+            switch result {
+            case .success:
+                completion(result)
+            case .failure:
+                self?.fallback.load(completion: completion)
+            }
+        }
     }
 }
 
@@ -42,9 +49,34 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 
         wait(for: [exp], timeout: 1)
     }
+
+    func test_load_deliversFallbackFeedOnPrimaryLoaderFailure() {
+        let fallbackFeed = uniqueFeed
+        let sut = makeSUT(primaryResult: .failure(anyNSError), fallbackResult: .success(fallbackFeed))
+
+        let exp = expectation(description: "Wait for load completion")
+
+        sut.load { result in
+            switch result {
+            case let .success(receivedFeed):
+                XCTAssertEqual(receivedFeed, fallbackFeed)
+
+            case .failure:
+                XCTFail("Expected successful load feed result, got \(result) instead")
+            }
+
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1.0)
+    }
 }
 
 private extension FeedLoaderWithFallbackCompositeTests {
+    var anyNSError: NSError {
+        NSError(domain: "any error", code: 0)
+    }
+
     var uniqueFeed: [FeedImage] {
         [FeedImage(id: UUID(), description: "Description", location: "Location", url: URL(string: "https://any-url.com")!)]
     }
