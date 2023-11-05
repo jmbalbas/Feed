4a0fef98fe4d345b2c23f6609cda44b721d525ac
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-11-05 18:54:10 +0100

    Move `FeedImageDataCache` to the `Feed Feature production group

---
+++ EssentialFeed/Feed/Feed Feature/FeedImageDataCache.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageDataCache.swift
+//  Feed
+//
+//  Created by Juan Santiago Martín Balbás on 5/11/23.
+//
+
+import Foundation
+
+public protocol FeedImageDataCache {
+    typealias Result = Swift.Result<Void, Error>
+
+    func save(_ data: Data, for url: URL, completion: @escaping (Result) -> Void)
+}
