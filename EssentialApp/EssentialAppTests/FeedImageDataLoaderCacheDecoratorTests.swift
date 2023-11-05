d901b27e5815d0eff558b614d3ee907f26fd1f32
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-11-05 18:45:23 +0100

    Extract `FeedImageDataLoaderSpy` into a shared scope to remove duplication

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -24,57 +24,57 @@ class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
 final class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
     func test_init_doesNotLoadImageData() {
         let (_, loader) = makeSUT()
-
+        
         XCTAssert(loader.loadedURLs.isEmpty, "Expected no loaded URLs")
     }
-
+    
     func test_loadImageData_loadsFromLoader() {
         let url = anyURL
         let (sut, loader) = makeSUT()
-
+        
         _ = sut.loadImageData(from: url) { _ in }
-
+        
         XCTAssertEqual(loader.loadedURLs, [url], "Expected to load URL from loader")
     }
-
+    
     func test_cancelLoadImageData_cancelsLoaderTask() {
         let url = anyURL
         let (sut, loader) = makeSUT()
-
+        
         let task = sut.loadImageData(from: url) { _ in }
         task.cancel()
-
+        
         XCTAssertEqual(loader.cancelledURLs, [url], "Expected to cancel URL loading from loader")
     }
-
+    
     func test_loadImageData_deliversDataOnLoaderSuccess() {
         let imageData = anyData
         let (sut, loader) = makeSUT()
-
+        
         expect(sut, toCompleteWith: .success(imageData), when: {
             loader.complete(with: imageData)
         })
     }
-
+    
     func test_loadImageData_deliversErrorOnLoaderFailure() {
         let (sut, loader) = makeSUT()
-
+        
         expect(sut, toCompleteWith: .failure(anyNSError), when: {
             loader.complete(with: anyNSError)
         })
     }
+}
 
-    // MARK: - Helpers
-
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImageDataLoader, loader: LoaderSpy) {
-        let loader = LoaderSpy()
+private extension FeedImageDataLoaderCacheDecoratorTests {
+    func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImageDataLoader, loader: FeedImageDataLoaderSpy) {
+        let loader = FeedImageDataLoaderSpy()
         let sut = FeedImageDataLoaderCacheDecorator(decoratee: loader)
         trackForMemoryLeaks(loader, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, loader)
     }
 
-    private func expect(
+    func expect(
         _ sut: FeedImageDataLoader,
         toCompleteWith expectedResult: FeedImageDataLoader.Result,
         when action: () -> Void,
@@ -101,35 +101,5 @@ final class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
         action()
 
         wait(for: [exp], timeout: 1.0)
-    }
-
-    private class LoaderSpy: FeedImageDataLoader {
-        private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
-
-        private(set) var cancelledURLs = [URL]()
-
-        var loadedURLs: [URL] {
-            messages.map { $0.url }
-        }
-
-        private struct Task: FeedImageDataLoaderTask {
-            let callback: () -> Void
-            func cancel() { callback() }
-        }
-
-        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-            messages.append((url, completion))
-            return Task { [weak self] in
-                self?.cancelledURLs.append(url)
-            }
-        }
-
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-
-        func complete(with data: Data, at index: Int = 0) {
-            messages[index].completion(.success(data))
-        }
     }
 }
