4ac23fb00bf66c7d8e4a61632867903e6a824910
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-11-22 16:22:11 +0100

    Inline parameter

--- EssentialFeed/FeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/FeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -23,7 +23,8 @@ final class FeedPresenterTests: XCTestCase {
 }
 
 private extension FeedPresenterTests {
-    func localized(_ key: String, table: String = "Feed", file: StaticString = #file, line: UInt = #line) -> String {
+    func localized(_ key: String , file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
         let bundle = Bundle(for: FeedPresenter.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
         if value == key {
