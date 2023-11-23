2d06dfc84f6616f66f5b42fe805a5840eb114ad6
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-11-23 16:09:49 +0100

    Implement Image Comments UI

---
+++ EssentialFeed/FeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,33 @@
+//
+//  ImageCommentCellController.swift
+//  FeediOS
+//
+//  Created by Juan Santiago Martín Balbás on 23/11/23.
+//
+
+import Feed
+import UIKit
+
+public class ImageCommentCellController: CellController {
+    private let model: ImageCommentViewModel
+
+    public init(model: ImageCommentViewModel) {
+        self.model = model
+    }
+
+    public func view(in tableView: UITableView) -> UITableViewCell {
+        let cell: ImageCommentCell = tableView.dequeueReusableCell()
+        cell.messageLabel.text = model.message
+        cell.dateLabel.text = model.date
+        cell.usernameLabel.text = model.username
+        return cell
+    }
+    
+    public func preload() {
+
+    }
+    
+    public func cancelLoad( ) {
+
+    }
+}
