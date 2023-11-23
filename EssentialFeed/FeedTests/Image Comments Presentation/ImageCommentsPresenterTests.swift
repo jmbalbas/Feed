6b40c5203af1760ab531811d366fed2ab78a57be
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-11-23 11:49:51 +0100

    Test that image comments title is localized

--- EssentialFeed/FeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/FeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -12,6 +12,42 @@ final class ImageCommentsPresenterTests: XCTestCase {
     func test_title_isLocalized() {
         XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
     }
+
+    func test_map_createsViewModels() {
+        let now = Date()
+        let comments = [
+            ImageComment(
+                id: UUID(),
+                message: "A message",
+                createdAt: now.adding(minutes: -5),
+                username: "A username"
+            ),
+            ImageComment(
+                id: UUID(),
+                message: "Another message",
+                createdAt: now.adding(days: -1),
+                username: "Another username"
+            )
+        ]
+
+        let viewModel = ImageCommentsPresenter.map(comments)
+
+        XCTAssertEqual(
+            viewModel.comments,
+            [
+                ImageCommentViewModel(
+                    message: "A message",
+                    date: "hace 5 minutos",
+                    username: "A username"
+                ),
+                ImageCommentViewModel(
+                    message: "Another message",
+                    date: "hace 1 día",
+                    username: "Another username"
+                )
+            ]
+        )
+    }
 }
 
 private extension ImageCommentsPresenterTests {
