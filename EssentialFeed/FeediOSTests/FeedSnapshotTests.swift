ea410e50eb4d788a207a04b75c2de897dd4eef57
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-11-23 11:34:23 +0100

    Update `FeedSnapshotTests` to assert on snapshots and fix failing tests

--- EssentialFeed/FeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/FeediOSTests/FeedSnapshotTests.swift
@@ -15,17 +15,17 @@ final class FeedSnapshotTests: XCTestCase {
 
         sut.display(emptyFeed)
 
-        record(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_FEED_light")
-        record(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_FEED_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
     }
 
     func test_feedWithContent() {
         let sut = makeSUT()
 
         sut.display(feedWithContent)
-
-        record(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_CONTENT_light")
-        record(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_CONTENT_dark")
+        
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_CONTENT_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_CONTENT_dark")
     }
 
     func test_feedWithErrorMessage() {
@@ -37,8 +37,8 @@ final class FeedSnapshotTests: XCTestCase {
         error message
         """))
 
-        record(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_ERROR_MESSAGE_light")
-        record(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_ERROR_MESSAGE_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_ERROR_MESSAGE_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_ERROR_MESSAGE_dark")
     }
 
     func test_feedWithFailedImageLoading() {
@@ -46,8 +46,8 @@ final class FeedSnapshotTests: XCTestCase {
 
         sut.display(feedWithFailedImageLoading)
 
-        record(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_FAILED_IMAGE_LOADING_light")
-        record(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_FAILED_IMAGE_LOADING_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
     }
 }
 
@@ -126,6 +126,8 @@ private class ImageStub: FeedImageCellControllerDelegate {
         } else {
             controller?.display(ResourceErrorViewModel(message: "any"))
         }
+
+        controller?.display(ResourceLoadingViewModel(isLoading: false))
     }
 
     func didCancelImageRequest() {
