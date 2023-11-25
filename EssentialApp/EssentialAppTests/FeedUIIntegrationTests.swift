682aa25718f4b999bf6ff375878a46fece7c3883
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-11-25 12:13:39 +0100

    Release cell reference on `prepareForReuse` as a fix for iOS 15+ cell lifecycle changes.

--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -331,6 +331,21 @@ final class FeedUIIntegrationTests: XCTestCase {
         XCTAssertFalse(view0.isShowingImageLoadingIncator, "Expected no loading indicator when image loads successfully after view becomes visible again")
     }
     
+    func test_feedImageView_doesNotShowDataFromPreviousRequestWhenCellIsReused() throws {
+        let (sut, loader) = makeSUT()
+
+        sut.simulateAppearance()
+        loader.completeFeedLoading(with: [makeImage(), makeImage()])
+
+        let view0 = try XCTUnwrap(sut.simulateFeedImageViewVisible(at: 0))
+        view0.prepareForReuse()
+
+        let imageData0 = try XCTUnwrap(UIImage.make(withColor: .red).pngData())
+        loader.completeImageLoading(with: imageData0, at: 0)
+
+        XCTAssertEqual(view0.renderedImage, .none, "Expected no image state change for reused view once image loading completes successfully")
+    }
+
     func test_feedImageView_doesNotRenderLoadedImageWhenNotVisibleAnymore() {
         let (sut, loader) = makeSUT()
         sut.simulateAppearance()
