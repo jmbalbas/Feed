05de137e6ff33443d2d28daea6872682b903794a
Juan Santiago Balbás Martín <jmbalbas@gmail.com>
2023-11-28 13:03:40 +0100

    Add "Load more" cell controller with loading indicator

---
+++ EssentialFeed/FeediOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -0,0 +1,27 @@
+//
+//  LoadMoreCellController.swift
+//  FeediOS
+//
+//  Created by Juan Santiago Martín Balbás on 28/11/23.
+//
+
+import Feed
+import UIKit
+
+public class LoadMoreCellController: NSObject, UITableViewDataSource {
+    private let cell = LoadMoreCell()
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        cell
+    }
+}
+
+extension LoadMoreCellController: ResourceLoadingView {
+    public func display(_ viewModel: ResourceLoadingViewModel) {
+        cell.isLoading = viewModel.isLoading
+    }
+}
--- EssentialFeed/FeediOSTests/Feed UI/FeedSnapshotTests.swift
+++ EssentialFeed/FeediOSTests/Feed UI/FeedSnapshotTests.swift
@@ -28,6 +28,15 @@ final class FeedSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_FAILED_IMAGE_LOADING_light")
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
     }
+
+    func test_feedWithLoadMoreIndicator() {
+        let sut = makeSUT()
+
+        sut.display(feedWithLoadMoreIndicator)
+
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_LOAD_MORE_INDICATOR_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_LOAD_MORE_INDICATOR_dark")
+    }
 }
 
 private extension FeedSnapshotTests {
@@ -58,6 +67,19 @@ private extension FeedSnapshotTests {
                 location: "Brighton Seafront",
                 image: nil
             )
+        ]
+    }
+
+    var feedWithLoadMoreIndicator: [CellController] {
+        let stub = feedWithContent.last!
+        let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub, selection: {})
+        stub.controller = cellController
+
+        let loadMore = LoadMoreCellController()
+        loadMore.display(ResourceLoadingViewModel(isLoading: true))
+        return [
+            CellController(id: UUID(), cellController),
+            CellController(id: UUID(), loadMore)
         ]
     }
 
